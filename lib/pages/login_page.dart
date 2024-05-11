93cc63a4dfd6f847e1b25ec6b7fa66d8497d5327
TS_YESWANTH <tsyeswanth.eee2022@citchennai.net>
2024-05-11 19:57:53 +0530

    Day 2 Completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/login_page.png",
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(height: 16.0),
+        const Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Enter Username", labelText: "Username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        ElevatedButton(
+          child: Text("Login Page"),
+          style: TextButton.styleFrom(),
+          onPressed: () {
+            print("Hello World!");
+          },
+        )
+      ],
+    ));
+  }
+}
